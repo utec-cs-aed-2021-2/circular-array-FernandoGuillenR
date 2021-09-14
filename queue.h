2ac9c785b39f368eb8c52e50fd89ce0c8b8d8542
FernandoGuillenR <66321787+FernandoGuillenR@users.noreply.github.com>
2021-09-14 00:40:23 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -2,5 +2,21 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
+    public:
     
-};
+    template<typename T>
+    void enqueue(T data){
+        if(is_full()){
+            resize();
+        }
+        int rear = next(rear);
+        array[rear] = data;
+    }
+    
+    template<typename T>
+    void dequeue(T data){
+        if(is_empty()) throw("is empty");
+        front = next(front);
+    }  
+};  
+  
