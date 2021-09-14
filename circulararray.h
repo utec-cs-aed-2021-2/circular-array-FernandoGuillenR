41a7c8d5df1b472b1b83b3a498addc31bf2a921e
FernandoGuillenR <66321787+FernandoGuillenR@users.noreply.github.com>
2021-09-14 00:37:24 -0500

    Update circulararray.h

--- circulararray.h
+++ circulararray.h
@@ -33,6 +33,140 @@ private:
     int prev(int);
 };
 
+// IMPLEMENTACIONES
+
+
+template <class T>
+void CircularArray<T>::push_front(T data)
+{
+  if(is_empty())
+  {
+    array[front]=data;
+  }
+
+  else if(is_full())
+  {
+    T *new_array = new T [capacity+10];
+    for(int i = front; i < rear; i++)
+    {
+      new_array[i+1] = array[i];
+    }
+    new_array[front] = data;
+    array = new_array;
+  }
+  else
+  {
+    for(int i=rear; i > front; i--)
+    {
+      array[i+1] = array[i];
+    }
+    array[front]=data;
+  }
+}
+
+template <class T>
+void CircularArray<T>::push_back(T data)
+{
+  if(is_empty())
+  {
+    array[back] = data;
+  }
+  else if(is_full())
+  {
+    T *new_array = new T[capacity+10];
+    for(int i = front; i < rear; i++)
+    {
+      array[i] = new_array[i];
+    }
+    new_array[back+1] = data;
+    array = new_array;
+  }
+  else{
+    array[back+1] = data;
+  }
+}
+
+template <class T>
+void CircularArray<T>::pop_front(T data)
+{
+  if(is_empty())
+  {
+    cout << "error"; << endl;
+  }
+  else if
+  {
+     T *new_array = new T [capacity];
+     for(int i = front; i < rear; i++)
+     {
+       array[i+1] = new_array[i];
+     }
+     array = new_array;
+  }
+
+}
+
+template <class T>
+void CircularArray<T>::pop_back(T data)
+{
+  if(is_empty())
+  {
+    array[back] = data;
+  }
+  else if(is_full())
+  {
+    T *new_array = new T [capacity+10];
+    new_array[back+1] = data;
+    array = new_array;
+  }
+  else
+  {
+    array[back+1] = data;
+  }
+}
+
+
+template <class T>
+bool CircularArray<T>::is_full()
+{
+  return (back+1) % capacity == front;
+}
+
+template <class T>
+bool CircularArray<T>::is_empty()
+{
+  if(front == rear)
+  {
+    return true;
+  } 
+  else
+  {
+    return false;
+  }
+
+}
+
+
+// SIN TERMINAR 
+template <class T>
+int CircularArray<T>::size()
+{
+  int count = 0;
+  if(front == rear)
+  {
+    return 0;
+  }
+  else
+  {
+    for(int i = front; i < rear; i++){
+      count++;
+    }
+    return count;
+  }
+}
+
+
+// DEFAULTS
+
 template <class T>
 CircularArray<T>::CircularArray()
 {
@@ -70,6 +204,8 @@ string CircularArray<T>::to_string(string sep)
 {
     string result = ""; 
     for (int i = 0; i < size(); i++)
+    {
         result += std::to_string((*this)[i]) + sep;
+    }
     return result;    
 }
